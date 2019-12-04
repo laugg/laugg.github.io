fc8d82133b29fba9e6927a54cd17a788837f6b47
Laurent_G <laurent.guesdedegouvea@orange.com>
2019-12-04 14:43:37 +0100

    [rm] useless git init script

--- gh_init.sh
+++
@@ -1,9 +0,0 @@
-# This script will run the necessary git commands to point to GitHub
-
-git remote add origin git@github.com:g2g/talk-about-taskwarrior-and-bugwarrior
-
-git add .
-git commit -m "first commit"
-git push -u origin master
-
-rm gh_init.sh
